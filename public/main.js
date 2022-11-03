7554f31c5a1992ee4910855e03a9245d71830913
Nicolas Garaycochea <nicolas.garaycochea@gmail.com>
2022-11-03 13:06:56 -0700

    commit

--- public/main.js
+++ public/main.js
@@ -2,7 +2,8 @@ const taskContainer = document.querySelector('#task-container')
 const form = document.querySelector('form')
 const plant = document.getElementById("plant")
 
-const baseURL = `http://localhost:4000/api/task`
+const baseURL = ``
+// const baseURL = `http://localhost:4000/api/task`
 
 const tasksCallback = ({ data: tasks }) => displayTasks(tasks)
 const errCallback = err => console.log(err.response.data)
